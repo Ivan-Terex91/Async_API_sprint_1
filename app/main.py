4fd6728644074b1a4bb8e23cba73d0eaff346be4
Ivan Terehin <57365386+Ivan-Terex91@users.noreply.github.com>
2021-04-27 12:35:17 +0300

    Add middleware for cache. (#25)
    Co-authored-by: Ivan Terehin <ivs@digift.ru>

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ import logging
 import aioredis
 import uvicorn as uvicorn
 from elasticsearch import AsyncElasticsearch
-from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import ORJSONResponse, RedirectResponse
 
 from api.v1 import film, genre, person
-from core import config
+from core import config, json
 from core.logger import LOGGING
+from core.utils import async_iterator_wrapper
 from db import elastic, redis
+from db.redis import RedisStorage
 
 app = FastAPI(
     title=config.PROJECT_NAME,
@@ -19,6 +21,29 @@ app = FastAPI(
 )
 
 
+@app.middleware("http")
+async def cache(request: Request, call_next):
+    """Middleware для кэширования"""
+    redis_adapter = await redis.get_redis()
+    redis_cache = RedisStorage(redis_adapter)
+    key = request.get("raw_path") + request.get("query_string")
+    data_in_cache = await redis_cache.get_data_in_redis(key=key)
+
+    if data_in_cache:
+        return ORJSONResponse(content=json.loads(data_in_cache))
+
+    response = await call_next(request)
+    if response.status_code == 307:
+        return RedirectResponse(url=response.headers.get("location"))
+
+    resp_body = [section async for section in response.__dict__["body_iterator"]]
+    response.__setattr__("body_iterator", async_iterator_wrapper(resp_body))
+    resp_body = resp_body[0]
+
+    await redis_cache.put_data_from_redis(key=key, value=json.loads(resp_body))
+    return response
+
+
 @app.on_event("startup")
 async def startup():
     """
@@ -26,7 +51,9 @@ async def startup():
     Подключиться можем при работающем event-loop
     Поэтому логика подключения происходит в асинхронной функции
     """
-    redis.redis = await aioredis.create_redis_pool(config.REDIS_DSN, minsize=10, maxsize=20)
+    redis.redis = await aioredis.create_redis_pool(
+        config.REDIS_DSN, minsize=10, maxsize=20, encoding="utf-8"
+    )
     elastic.es = AsyncElasticsearch(hosts=[config.ELASTIC_DSN])
 
 
